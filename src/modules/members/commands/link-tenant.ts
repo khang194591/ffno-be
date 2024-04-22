103267e1e3cf3b3910567c3b6980c15ff6c48984
khang <khang194591@gmail.com>
2024-04-22 17:58:07 +0700

    update

--- src/modules/members/commands/link-tenant.ts
+++ src/modules/members/commands/link-tenant.ts
@@ -1,11 +1,13 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PrismaService } from 'src/config';
 import { MemberRole } from 'src/libs/constants';
+import { MemberResDto } from 'src/libs/dto';
+import { NotificationService } from 'src/modules/services/notification.service';
 import { MemberService } from '../member.service';
 
 export class LinkTenantCommand {
   constructor(
-    public readonly staffId: string,
+    public readonly currentMember: MemberResDto,
     public readonly data: string,
   ) {}
 }
@@ -15,19 +17,33 @@ export class LinkTenantHandler implements ICommandHandler<LinkTenantCommand> {
   constructor(
     private readonly prisma: PrismaService,
     private readonly memberService: MemberService,
+    private readonly notificationService: NotificationService,
   ) {}
 
   async execute(query: LinkTenantCommand): Promise<any> {
-    const { staffId, data } = query;
-    const member = await this.memberService.getMemberOrThrow(data);
+    const { currentMember, data } = query;
+    const member = await this.memberService.getMember(data);
 
     // TODO: Handle send email or OTP
-    await this.prisma.memberContacts.create({
-      data: {
-        type: MemberRole.TENANT,
-        contactId: staffId,
-        contactWithId: member.id,
-      },
-    });
+    if (member) {
+      await this.prisma.memberContacts.create({
+        data: {
+          type: MemberRole.TENANT,
+          contactId: currentMember.id,
+          contactWithId: member.id,
+        },
+      });
+    } else {
+      await this.notificationService.sendEmail({
+        to: data,
+        subject: 'Invitation to Join FFNO',
+        template: 'invitation-tenant',
+        context: {
+          tenantName: data,
+          landlordName: currentMember.name,
+          invitationLink: 'http://localhost:8000/auth/sign-up?invi',
+        },
+      });
+    }
   }
 }
